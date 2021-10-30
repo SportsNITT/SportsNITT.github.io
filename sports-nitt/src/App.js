037c1ec9284d95dfec336ca272ab27ff4e429bc8
Hetvi Bhansali <63287315+hetvi955@users.noreply.github.com>
2021-10-30 12:48:47 +0530

    add achievement cards

--- sports-nitt/src/App.js
+++ sports-nitt/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import Achievements from './pages/achievements/achievements';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import ResponsiveGrid from '../src/pages/achievements/cards';
 
 function App() {
   return (
     <div className="App">
-      <Achievements
-     
-        />
-
+      <Achievements/>
+      <ResponsiveGrid/>
     </div>
   );
 }
